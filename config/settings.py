1886367083356f8dac792aa89c2ba8708a7ff071
Mohammad Erfan Pouladzadeh <m.erfanpouladzadeh@yahoo.com>
2024-08-30 14:20:17 +0330

    Adding CORS

--- config/settings.py
+++ config/settings.py
@@ -197,6 +197,6 @@ SPECTACULAR_SETTINGS = {
 
 # CORS HTTP
 CORS_ALLOWED_ORIGINS = [
-    "http://localhost:3000",
-    "http://127.0.0.1:3000",
+    "http://localhost:5173",
+    "http://127.0.0.1:5173",
 ]
