e440fba23f3c8c64402ed540b57644fb4b29370f
MErfanPld <m.erfanpouladzadeh@yahoo.com>
2024-09-02 08:13:48 +0330

    edited ...

--- config/settings.py
+++ config/settings.py
@@ -179,9 +179,17 @@ CHANNEL_LAYERS = {
 }
 
 REST_FRAMEWORK = {
-    # YOUR SETTINGS
+    "DEFAULT_AUTHENTICATION_CLASSES": (
+        "rest_framework_simplejwt.authentication.JWTAuthentication",
+        "rest_framework.authentication.SessionAuthentication",
+    ),
+    'DEFAULT_THROTTLE_RATES': {
+        'login': '2/day'
+    },
     'DEFAULT_SCHEMA_CLASS': 'drf_spectacular.openapi.AutoSchema',
+    'DEFAULT_FILTER_BACKENDS': ['django_filters.rest_framework.DjangoFilterBackend']
 }
+
 
 # SWAGGER
 SPECTACULAR_SETTINGS = {
