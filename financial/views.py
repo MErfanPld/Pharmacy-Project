226a75f8f4edc652affbb2db9e44b2bd3e0b5ae7
Mohammad Erfan Pouladzadeh <m.erfanpouladzadeh@yahoo.com>
2024-10-26 14:32:14 +0330

    Edited Financial API

--- financial/views.py
+++ financial/views.py
@@ -10,26 +10,17 @@ class TransactionListCreateAPIView(generics.ListCreateAPIView):
     queryset = Transaction.objects.all()
     serializer_class = TransactionSerializer
 
+
 class TransactionDetailAPIView(generics.RetrieveUpdateDestroyAPIView):
     queryset = Transaction.objects.all()
     serializer_class = TransactionSerializer
 
-class FinancialReportCreateAPIView(APIView):
-    def post(self, request):
-        serializer = FinancialReportSerializer(data=request.data)
-        if serializer.is_valid():
-            report = serializer.save()
-            report.generate_report()
-            return Response(FinancialReportSerializer(report).data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+class FinancialReportCreateAPIView(generics.ListCreateAPIView):
+    queryset = FinancialReport.objects.all()
+    serializer_class = FinancialReportSerializer
 
 
-class FinancialReportDetailAPIView(APIView):
-    def get(self, request, pk):
-        try:
-            report = FinancialReport.objects.get(pk=pk)
-        except FinancialReport.DoesNotExist:
-            return Response({'error': 'گزارش مالی پیدا نشد'}, status=status.HTTP_404_NOT_FOUND)
-
-        serializer = FinancialReportSerializer(report)
-        return Response(serializer.data)
+class FinancialReportDetailAPIView(generics.ListCreateAPIView):
+    queryset = FinancialReport.objects.all()
+    serializer_class = FinancialReportSerializer
